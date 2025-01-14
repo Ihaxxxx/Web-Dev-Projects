77a2bc4d067d34f338fb18138c663bb0c6b7c6cb
Ihaxxxx <mubashir.mandvia@gmail.com>
2025-01-14 18:43:39 +0500

    video 1 done

--- A+ Algorith/index.js
+++ A+ Algorith/index.js
@@ -1,17 +1,18 @@
 
 
-function removeFromArray(arr,element){
+function removeFromArray(arr, element) {
     // traversing backwards
-    for (let index = arr.length-1; index >= 0; index--) {
+    for (let index = arr.length - 1; index >= 0; index--) {
         if (arr[index] == element) {
-            arr.splice(index,1);
+            arr.splice(index, 1);
         }
     }
 }
 
 // calc guess 
-function heuristic(a,b){
-    var distance = dist(a.i,a.j,b.i,b.j)
+function heuristic(a, b) {
+    // var distance = dist(a.i, a.j, b.i, b.j)
+    var distance = abs(a.i-b.i) + abs(a.j-b.j)
     return distance
 }
 
@@ -21,15 +22,16 @@ var grid = new Array(col)
 
 var openSet = []
 var closedSet = []
-var start ;
+var start;
 var end;
-var width ;
-var height ;
+var width;
+var height;
 var previous = undefined
 var path = [];
+var noSolution = false
 
 
-function Spot(i,j){
+function Spot(i, j) {
     // get the position from where to draw
     this.i = i
     this.j = j
@@ -37,14 +39,23 @@ function Spot(i,j){
     this.g = 0
     this.h = 0
     this.neighbours = []
+    this.previous = undefined
+    this.wall = false
 
-    this.show = function(col){
+    if (random(1) < 0.3 ) {
+        this.wall = true
+    }
+
+    this.show = function (col) {
         fill(col)
+        if (this.wall) {
+            fill(0)
+        }
         noStroke()
-        rect(this.i*w,this.j*h,w-1,h-1)
+        rect(this.i * w, this.j * h, w - 1, h - 1)
     }
     // adding neighbours
-    this.addneighbours = function(grid){
+    this.addneighbours = function (grid) {
         var i = this.i;
         var j = this.j;
         if (i < col - 1) {
@@ -58,34 +69,36 @@ function Spot(i,j){
         }
         if (j > 0) {
             this.neighbours.push(grid[i][j - 1]);
-        }   
+        }
     }
 }
 
-function setup(){
+function setup() {
     createCanvas(400, 400);
 
-    w = width/col
-    h = height/row
+    w = width / col
+    h = height / row
 
-    for (let i = 0; i < col ; i++) {
+    for (let i = 0; i < col; i++) {
         grid[i] = new Array(row)
     }
     console.log(grid)
     // adding spots
-    for (let i= 0; i< col; i++) {
-        for (let j = 0; j < row; j++) {            
-            grid[i][j] = new Spot(i,j)
+    for (let i = 0; i < col; i++) {
+        for (let j = 0; j < row; j++) {
+            grid[i][j] = new Spot(i, j)
         }
     }
     // adding neighboiurs
-    for (let i= 0; i< col; i++) {
-        for (let j = 0; j < row; j++) {            
+    for (let i = 0; i < col; i++) {
+        for (let j = 0; j < row; j++) {
             grid[i][j].addneighbours(grid)
         }
     }
     start = grid[0][0];
-    end = grid[col-1][row-1]
+    end = grid[col - 1][row - 1] // row
+    start.wall = false
+    end.wall = false
     openSet.push(start)
 }
 
@@ -93,7 +106,7 @@ function setup(){
 setup()
 
 
-function draw(){
+function draw() {
     if (openSet.length > 0) {
         // finding the lowest F
         var lowestIndex = 0
@@ -106,42 +119,41 @@ function draw(){
         var current = openSet[lowestIndex]
 
         if (current === end) {
-            // find path
-            path = []
-            var temp = current
-            while(temp.previous){
-                path.push(temp.previous);
-                temp = temp.previous
-            }
+            noLoop()
             console.log("Done")
         }
         // deleting the dealt node and moving it to the closed Set
-        removeFromArray(openSet,current)
+        removeFromArray(openSet, current)
         closedSet.push(current)
         var neighbours = current.neighbours
         for (let i = 0; i < neighbours.length; i++) {
             var neighboiur = neighbours[i]
             // checking possible G score
-            if (!closedSet.includes(neighboiur)) {
-                var tempG =  current.g + 1
+            if (!closedSet.includes(neighboiur) && !neighboiur.wall) {
+                var tempG = current.g + 1
                 // check if this exist in open set to see if tit is a batter G
                 if (openSet.includes(neighboiur)) {
                     // finding the optimal G score
                     if (tempG < neighboiur.g) {
                         neighboiur.g = tempG
                     }
-                }else{
+                } else {
                     neighboiur.g = tempG
                     openSet.push(neighboiur);
                 }
-                neighboiur.h = heuristic(neighboiur,end)
+                // total distance a guess
+                neighboiur.h = heuristic(neighboiur, end)
+                // formula to calculate cost
                 neighboiur.f = neighboiur.g + neighboiur.h
                 neighboiur.previous = current // where thje neighboiur came from 
             }
         }
 
-    }else{
+    } else {
+        noSolution = true
+        console.log("nos solution")
         // no solution
+        noLoop()
     }
 
 
@@ -150,23 +162,36 @@ function draw(){
 
     // debug
 
-    for (let i = 0; i < col ; i++) {
-        for (let j = 0; j < row; j++) {            
+    for (let i = 0; i < col; i++) {
+        for (let j = 0; j < row; j++) {
             grid[i][j].show(color(255))
         }
     }
 
     // closed set position
     for (let index = 0; index < closedSet.length; index++) {
-        closedSet[index].show(color(255,0,0))
+        closedSet[index].show(color(255, 0, 0))
     }
 
+    // find path
+    if (!noSolution) {
+        path = []
+        var temp = current
+        // run this loop until there is no previous element
+        while (temp.previous) {
+            // pushing the previous paths in a temp variable 
+            path.push(temp.previous);
+            temp = temp.previous
+        }
+    }
     // open set position green
     for (let index = 0; index < openSet.length; index++) {
-        openSet[index].show(color(255,0,0))
+        openSet[index].show(color(0, 255, 0))
     }
-
+    // final path drawing  
     for (let index = 0; index < path.length; index++) {
-        path[index].show(color(0,0,255))
+        // console.log(index)
+        console.log(path[index])
+        path[index].show(color(0, 0, 255))
     }
 }
